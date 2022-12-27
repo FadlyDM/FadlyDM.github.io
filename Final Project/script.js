bb0386ec15bd8d001caacd4262e9341a70f670b6
FadlyDM <119001850+FadlyDM@users.noreply.github.com>
2022-12-27 21:23:53 +0800

    Add files via upload

---
+++ Final Project/script.js
@@ -0,0 +1,57 @@
+var settingsmenu = document.querySelector(".settings-menu");
+var settingsmenu2 = document.querySelector(".notif-menu");
+var darkBtn = document.getElementById("dark-btn");
+var b = false;
+
+function settingsMenuToggle(){
+    settingsmenu.classList.toggle("settings-menu-height")
+}
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if(localStorage.getItem("theme") == "dark"){
+        localStorage.setItem("theme", "light");
+    }
+    else{
+        localStorage.setItem("theme", "dark");
+    }
+}
+
+if(localStorage.getItem("theme") == "dark"){
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme")
+}
+else if(localStorage.getItem("theme") == "light"){
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme")
+}
+else{
+    localStorage.setItem("theme", "dark");
+}
+
+
+function settingsMenuToggle2(id){
+    settingsmenu2.classList.toggle("settings-menu-height");
+    var e = document.getElementById(id);
+    if(e.style.display == 'none')
+        e.style.display = 'block';
+    else
+    e.style.display = 'none';
+    b = true;
+
+    var f = document.getElementById('notif');
+    if(f.style.display == 'none')
+        f.style.display = 'block';
+    else
+    f.style.display = 'none';
+    b = false;
+}
+
+
+function foo(){
+    var e = document.getElementById('foo');
+    if(!b) e.style.display = 'none';
+    b=false;
+}
+
